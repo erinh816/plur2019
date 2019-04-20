e94545ea02a7c420abfcf99fc1c91f27b3889a79
Silu Hu <erinh816@gmail.com>
2019-04-20 14:11:36 -0400

    start plur.js

---
+++ assets/javaScript/plur2019.js
@@ -0,0 +1,19 @@
+var events = []
+var pages = []
+// var activePage=1 
+// var createPagesAlready = false
+// var featured = ["coachella", "electric zoo", "ultra", "electric daisy", "creamfields", "electric forest", "holy ship", "escape", "countdown", "burning man", "lollapalooza", "moonrise festival", "crssd", "tomorrowland"]
+// var liked
+
+var eventParameters = {
+    client: '', //your assigned client API key
+    locationIds: '38', //what is 38???
+    startDate: '',
+    endDate: ''
+}
+
+function getEvents() {
+    pages = []
+    events = []
+
+}
